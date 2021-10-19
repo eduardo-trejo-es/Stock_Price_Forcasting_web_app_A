71b7c18e1907ec4b04a0602aa6c34a02df58d7bf
eduardo-trejo-es <alfredo.trejo.es@outlook.com>
2021-10-19 10:22:23 -0500

    hello world setup

--- website/views.py
+++ website/views.py
@@ -7,3 +7,8 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html", user="hello")
+
+
+@views.route('/about')
+def about():
+    return render_template("about_template.html")
