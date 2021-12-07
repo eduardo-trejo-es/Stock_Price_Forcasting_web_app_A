4c2b539d5effdcca0beb6a81ec95695618c41102
Alfredo Trejo <74158121+eduardo-trejo-es@users.noreply.github.com>
2021-12-07 07:58:08 -0600

    Date entry and RNN algo added

--- website/views.py
+++ website/views.py
@@ -1,8 +1,20 @@
 from flask import Blueprint, render_template, request, flash
 from StockPricePredictionFIles.ForcastingGeneScript import PriceForcaster
+from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template, session
+from flask_wtf import FlaskForm
+from wtforms.fields import DateField
+from wtforms.validators import DataRequired
+from wtforms import validators, SubmitField
+
 
 views = Blueprint('views', __name__)
 ForcastingObject = PriceForcaster()
+
+class InfoForm(FlaskForm):
+    startdate = DateField('Start Date', format='%Y-%m-%d', validators=(validators.DataRequired(),))
+    enddate = DateField('End Date', format='%Y-%m-%d', validators=(validators.DataRequired(),))
+    submit = SubmitField('Submit')
 
 Company_Selected="null"
 @views.route('/', methods=['POST', 'GET'])
@@ -16,15 +28,16 @@ def home():
         try:
             Company_Selected = request.form["AAPL_Val"]
         except:
-            pass
-                
-
-
-        #Company_Selected = request.form.get("dropdown_MenuButton")
-        #Company_Selected = request.form["dropdown_MenuButton"]
+            pass          
     else:
         Company_Selected = "null"
-    return render_template("home.html", value=ForcastingObject.Gettingforcasting(Company_Selected))
+    
+    form = InfoForm()
+    if form.validate_on_submit():
+        session['startdate'] = form.startdate.data
+        session['enddate'] = form.enddate.data
+        #return redirect('date')
+    return render_template("home.html", value=ForcastingObject.Gettingforcasting(Company_Selected),form=form)
 
 
 @views.route('/about')
